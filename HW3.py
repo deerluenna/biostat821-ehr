9a4115077be6dbc2104775e5a5b5f64530ef238b
Luenna Wu <mw514@duke.edu>
2022-02-08 23:29:23 -0500

    Made changes according to requests

--- HW3.py
+++ HW3.py
@@ -3,11 +3,6 @@ from datetime import *
 from typing import Tuple
 
 
-print('Enter your patient file name:')
-filename_patient = input()
-print('Enter your lab file name')
-filename_lab = input()
-
 """I chose a list of lists for my data structure for the following reason:
 A list can hold a mix of different kinds of data types, including multiple 
 charatcteristics and test information of the same patient, even though 
@@ -20,7 +15,7 @@ to access the same type of data.
 """
 
 
-def parse_data(filename: str) -> [list[list]]:
+def parse_data(filename: str) -> list[list[str]]:
     file = open(filename, 'r')
     text = file.readlines()
     # print(text)
@@ -35,16 +30,9 @@ def parse_data(filename: str) -> [list[list]]:
     return list_of_list
 
 
-parsed_patient_data = parse_data(filename_patient)
-parsed_lab_data = parse_data(filename_lab)
-
-
 # --- Return the number of patients older than a given age (years) --- #
-print('Enter the age to calculate:')
-age = input()
 
-
-def num_older_than(age, list_of_list_patient):
+def num_older_than(age: str, list_of_list_patient: list[list[str]]) -> int:
 
     age_col_idx = 0
     # print(list_of_list_patient[0])
@@ -59,27 +47,16 @@ def num_older_than(age, list_of_list_patient):
             list_of_list_patient[i][age_col_idx], "%Y-%m-%d %H:%M:%S.%f").year
         # print(p_birth)
 
-        if 2022-p_birth > float(age):
+        if datetime.now().year-p_birth > float(age):
             num += 1
 
-    print(f'Number of patients older than {age} is {num}')
+    print(num)
     return num
-
-
-num_older_than(age, parsed_patient_data)
 
 
 # --- Return a (unique) list of patients who have a given test with value above (">") or below ("<") a given level --- #
 
-print('To get the IDs of sick patient, enter the "lab test name" first:')
-lab = input()
-print('Enter either ">" or "<" for above or below:')
-gt_lt = input()
-print('Enter critical lab value:')
-value = input()
-
-
-def sick_patients(lab, gt_lt, value, list_of_list_lab):
+def sick_patients(lab: str, gt_lt: str, value: str, list_of_list_lab: [list[list[str]]]) -> str:
     lab_col_idx = 0
     for j in range(len(list_of_list_lab[0])):
         if list_of_list_lab[0][j] == 'LabName':
@@ -88,24 +65,50 @@ def sick_patients(lab, gt_lt, value, list_of_list_lab):
         if list_of_list_lab[0][j] == 'LabValue':
             value_col_idx = j
 
-    ID_larger = []
-    ID_smaller = []
+    id_larger = []
+    id_smaller = []
     for i in range(1, len(list_of_list_lab)):
         if list_of_list_lab[i][lab_col_idx] == lab:
             if gt_lt == '>' and list_of_list_lab[i][value_col_idx] > value:
-                ID_larger.append(list_of_list_lab[i][0])
+                id_larger.append(list_of_list_lab[i][0])
             elif gt_lt == '<' and list_of_list_lab[i][value_col_idx] < value:
-                ID_smaller.append(list_of_list_lab[i][0])
+                id_smaller.append(list_of_list_lab[i][0])
 
-    if ID_larger != []:
-        print(ID_larger)
-    if ID_smaller != []:
-        print(ID_smaller)
+    if id_larger != []:
+        print(id_larger)
+    elif gt_lt == '>':
+        print('No one is larger')
+
+    if id_smaller != []:
+        print(id_smaller)
+    elif gt_lt == '<':
+        print('No one is smaller')
 
     if gt_lt == '>':
-        return ID_larger
+        return id_larger
     else:
-        return ID_smaller
+        return id_smaller
 
 
-sick_patients(lab, gt_lt, value, parsed_lab_data)
+if __name__ == "__main__":
+    print('Enter your patient file name:')
+    filename_patient = input()
+    print('Enter your lab file name')
+    filename_lab = input()
+
+    parsed_patient_data = parse_data(filename_patient)
+    parsed_lab_data = parse_data(filename_lab)
+
+    print('Enter the age to calculate:')
+    age = input()
+
+    num_older_than(age, parsed_patient_data)
+
+    print('To get the IDs of sick patient, enter the "lab test name" first:')
+    lab = input()
+    print('Enter either ">" or "<" for above or below:')
+    gt_lt = input()
+    print('Enter critical lab value:')
+    value = input()
+
+    sick_patients(lab, gt_lt, value, parsed_lab_data)
