7f648bb3e970b75f196279209515401f2cd780aa
Luenna Wu <mw514@duke.edu>
2022-04-19 03:08:53 -0400

    Update SQL version

--- EHR_analysis.py
+++ EHR_analysis.py
@@ -1,6 +1,6 @@
+from EHR_objects import Patient, Lab
 from datetime import datetime
 from typing import List, Dict
-from EHR_objects import Patient, Lab
 import sqlite3
 import os
 
@@ -11,11 +11,11 @@ cur = conn.cursor()
 def parse_patient_data(filename_patient: str):
     """
     Parse the patient data file, create a table with patient information in the database.
-    Input: String of patient filename
-    Output: None
+    Input: String of the patient filename
+    Output: List of patient ids
 
     The computational complexity for the parser function is
-    N*(1+1) + N*(1+1) + N*1 + N*1 + N + N → 8N → N.
+    N*(1+1+1) → N.
     """
     # Create patient and lab tables in database
     cur.execute("DROP TABLE IF EXISTS patient")  # Drop the table if exists
@@ -25,26 +25,27 @@ def parse_patient_data(filename_patient: str):
     )
     with open(filename_patient, "r", encoding="utf-8-sig") as patient_file:
         text = patient_file.readlines()
-        for line in text:
-            col_in_line = line.strip().split("\t")
-            a, b, c, d, e, f, g = col_in_line
-            # print(a, b, c, d, e, f, g)
+        pat_id_list = []
+        for line in text:  # N times
+            col_in_line = line.strip().split("\t")  # 0(1)
+            a, b, c, d, e, f, g = col_in_line  # 0(1)
             cur.execute(
                 f"INSERT INTO patient VALUES('{a}','{b}','{c}','{d}','{e}','{f}','{g}')"
             )
-    # selected = cur.execute("SELECT PatientID FROM patient WHERE PatientDateOfBirth = '1947-12-28 02:45:40.547'").fetchall()
-    # print(selected)
+            pat_id_list.append(f"{a}")  # 0(1)
+    print(pat_id_list[1])
     conn.commit()
+    return pat_id_list
 
 
 def parse_lab_data(filename_lab: str):
     """
     Parse the lab data file, create a table with lab information in the database.
-    Input: String of lab filename
+    Input: String of the lab filename
     Output: None
 
     The computational complexity for the parser function is
-    N*(1+1) + N*(1+1) + N*1 + N*1 + N + N → 8N → N.
+    N*(1+1+1) → N.
     """
     cur.execute(
         "CREATE TABLE IF NOT EXISTS lab(PatientID TEXT, AdmissionID INT, LabName TEXT, \
@@ -53,120 +54,28 @@ def parse_lab_data(filename_lab: str):
 
     with open(filename_lab, "r", encoding="utf-8-sig") as lab_file:
         text = lab_file.readlines()
-        for line in text:
-            col_in_line = line.strip().split("\t")
-            a, b, c, d, e, f = col_in_line
-            # print(a, b, c, d, e, f)
-            cur.execute(f"INSERT INTO lab VALUES('{a}','{b}','{c}','{d}','{e}','{f}')")
+        for line in text:  # N times
+            col_in_line = line.strip().split("\t")  # 0(1)
+            a, b, c, d, e, f = col_in_line  # 0(1)
+            cur.execute(
+                f"INSERT INTO lab VALUES('{a}','{b}','{c}','{d}','{e}','{f}')"
+            )  # 0(1)
     conn.commit()
 
 
-# class Patient:
-#     """
-#     Patient class: each patient class object represents an unique patient,
-#     and contains information such as patient ID, gender, date of birth, and race.
-#     """
+def create_patient_class(pat_id_list) -> list[Patient]:
+    """
+    Create a list of patient objects based on a list of patient ids.
+    Input: List of patient ids
+    Output: List of patient objects
 
-#     def __init__(self, cursor, pat_id: str):
-#         """Initialize."""
-#         self.cursor = cursor
-#         self.pat_id = pat_id
-
-#     @property
-#     def gender(self):
-#         _gender = self.cursor.execute(
-#             f"SELECT PatientGender FROM patient WHERE PatientID = '{self.pat_id}'"
-#         ).fetchone()  # Fetch gender info
-#         return _gender[0]
-
-#     @property
-#     def race(self):
-#         _race = self.cursor.execute(
-#             f"SELECT PatientRace FROM patient WHERE PatientID = '{self.pat_id}'"
-#         ).fetchone()  # Fetch Race info
-#         return _race[0]
-
-#     @property
-#     def DOB(self):
-#         _DOB = self.cursor.execute(
-#             f"SELECT PatientDateOfBirth FROM patient WHERE PatientID = '{self.pat_id}';"
-#         ).fetchone() # Fetch DOB info
-#         return _DOB[0]
-
-#     @property
-#     def age(self):
-#         """Function to calculate age property."""
-#         p_birth = datetime.strptime(self.DOB, "%Y-%m-%d %H:%M:%S.%f").year
-#         return datetime.now().year - p_birth
-
-
-# class Lab:
-#     """
-#     Lab class: each lab class object represents a lab diagnosis result of a patient,
-#     and contains information such as patient ID, date of diagnosis, and the name, value and units of the lab examination.
-#     """
-
-#     def __init__(
-#         self,
-#         cursor,
-#         pat_id: str,
-#         adm_id: str,
-#         lab_name: str,
-#     ):
-#         self.cursor = cursor
-#         self.pat_id = pat_id
-#         self.adm_id = adm_id
-#         self.lab_name = lab_name
-
-#     @property
-#     def lab_value(self):
-#         _lab_value = self.cursor.execute(
-#             f"SELECT LabValue FROM lab WHERE PatientID = '{self.pat_id}' \
-#                 AND AdmissionID = '{self.adm_id}' AND LabName = '{self.lab_name}'"
-#         ).fetchone()
-#         return _lab_value[0]
-
-#     @property
-#     def units(self):
-#         _units = self.cursor.execute(
-#             f"SELECT LabUnits FROM lab WHERE PatientID = '{self.pat_id}' \
-#                 AND AdmissionID = '{self.adm_id}' AND LabName = '{self.lab_name}'"
-#         ).fetchone()
-#         return _units[0]
-
-#     @property
-#     def lab_date(self):
-#         _lab_date = self.cursor.execute(
-#             f"SELECT lab.LabDateTime FROM lab WHERE PatientID = '{self.pat_id}' \
-#                 AND AdmissionID = '{self.adm_id}' AND LabName = '{self.lab_name}'"
-#         ).fetchone()
-#         return _lab_date[0]
-
-
-def create_patient_class() -> list[Patient]:
-    patient_id_list = cur.execute("SELECT patient.PatientID FROM patient").fetchall()
-    # print(patient_id_list)
+    The computational complexity is N*(1+1) → N.
+    """
     list_of_patient_object = []
-    for patient_id in patient_id_list[1:]:  # N times
-        patient_object = Patient(cursor=cur, pat_id=patient_id[0])  # 0(1)
-        list_of_patient_object.append(patient_object)
-    #print(list_of_patient_object[0].pat_id, list_of_patient_object[0].gender)
+    for pat_id in pat_id_list[1:]:  # N times
+        patient_object = Patient(cursor=cur, pat_id=pat_id)  # 0(1)
+        list_of_patient_object.append(patient_object)  # 0(1)
     return list_of_patient_object
-
-
-def create_lab_class() -> list[Lab]:
-    lab_info_list = cur.execute(
-        "SELECT lab.PatientID, lab.AdmissionID, lab.LabName FROM lab"
-    ).fetchall()
-    list_of_lab_object = []
-    # print(lab_info_list[1])
-    for PatientID, AdmissionID, LabName in lab_info_list[1:]:  # N times
-        lab_object = Lab(
-            cursor=cur, pat_id=PatientID, adm_id=AdmissionID, lab_name=LabName
-        )  # 0(1)
-        list_of_lab_object.append(lab_object)
-    #print(list_of_lab_object[0].pat_id, list_of_lab_object[0].lab_name)
-    return list_of_lab_object
 
 
 ## Number of patients older
@@ -176,8 +85,7 @@ def num_older_than(age: float, list_of_patient_object: list[Patient]) -> int:
     Input: 1. Float of age 2. List of patient objects
     Output: Integer of number of patients
 
-    The computational complexity for the parser function is
-    N*(1+1) → 2N → N.
+    The computational complexity is N*1 → N.
     """
     num = 0
     for patient_object in list_of_patient_object:  # N times
@@ -188,41 +96,46 @@ def num_older_than(age: float, list_of_patient_object: list[Patient]) -> int:
 
 
 ## Sick patient IDs
-def sick_patients(
-    lab: str, gt_lt: str, value: float, list_of_lab_object: list[Lab]
-) -> set[str]:
+def sick_patients(lab: str, gt_lt: str, value: float) -> set[str]:
     """
     Find unique patient IDs with a lab value greater or less than
     the given value of the specified lab type.
 
-    The computational complexity is N*1 + N*1 → N.
+    Input: 1. String of lab name 2. String of operator 3.String of critical value
+    Output: Set of string of sick patient IDs
+
+    The computational complexity is 1 + N*1 + N*1 → 2N → N.
     """
 
-    id_larger = set()
-    id_smaller = set()
+    id_larger: set[str] = set()
+    id_smaller: set[str] = set()
 
     if (gt_lt != ">") & (gt_lt != "<"):  # 0(1)
         raise ValueError("Operator input should be either '>' or '<'")
     elif gt_lt == ">":  # 0(1)
-        for lab_object in list_of_lab_object:  # N times
-            if (
-                lab == lab_object.lab_name and float(lab_object.lab_value) > value
-            ):  # 0(1)
-                id_larger.add(lab_object.pat_id)
+        id_larger = cur.execute(
+            "SELECT distinct PatientID FROM lab WHERE (LabName = ?) AND (LabValue > ?)",
+            (lab, value),
+        ).fetchall()
+        output_id_larger = set()
+        for id in id_larger:  # N times
+            output_id_larger.add(id[0])
     elif gt_lt == "<":  # 0(1)
-        for lab_object in list_of_lab_object:  # N times
-            if (
-                lab == lab_object.lab_name and float(lab_object.lab_value) < value
-            ):  # 0(1)
-                id_smaller.add(lab_object.pat_id)
+        id_smaller = cur.execute(
+            "SELECT distinct PatientID FROM lab WHERE (LabName = ?) AND (LabValue < ?)",
+            (lab, value),
+        ).fetchall()
+        output_id_smaller = set()
+        for id in id_smaller:  # N times
+            output_id_smaller.add(id[0])
 
     if id_larger:  # 0(1)
-        return id_larger
+        return output_id_larger
     elif gt_lt == ">":
         return set([])
 
     if id_smaller:  # 0(1)
-        return id_smaller
+        return output_id_smaller
     elif gt_lt == "<":
         return set([])
 
@@ -231,33 +144,32 @@ def sick_patients(
 
 ## Age at first admission
 def age_first_adm(
-    list_of_patient_object: list[Patient],
-    list_of_lab_object: list[Lab],
     patient_id: str,
 ) -> int:
 
     """
     Calculate the age at first admission of the given patient.
 
+    Input: Patient ID
+    Output: Integer of age at first admission
+
     The computational complexity is N*N + N → N^2.
     """
+    p_birth = cur.execute(
+        "SELECT PatientDateOfBirth FROM patient WHERE (PatientID = ?)", (patient_id,)
+    ).fetchone()
+    # p_birth = Patient(cur, patient_id).DOB
+    # print(Patient(cur, patient_id).DOB)
+    adm_date = cur.execute(
+        "SELECT MIN(LabDateTime) FROM lab WHERE (PatientID = ?)", (patient_id,)
+    ).fetchone()
+    birth_yr = datetime.strptime(p_birth[0], "%Y-%m-%d %H:%M:%S.%f").year
+    lab_yr = datetime.strptime(adm_date[0], "%Y-%m-%d %H:%M:%S.%f").year
+    age_first_lab = lab_yr - birth_yr
 
-    for lab_object in list_of_lab_object:  # N times
-        if lab_object.pat_id == patient_id and int(lab_object.adm_id) == 1:  # 0(1)
-            age_first_lab = 0
-
-            for patient_object in list_of_patient_object:  # N times
-                if patient_object.pat_id == lab_object.pat_id:  # 0(1)
-                    p_birth = datetime.strptime(
-                        patient_object.DOB, "%Y-%m-%d %H:%M:%S.%f"
-                    ).year
-                    lab_yr = datetime.strptime(
-                        lab_object.lab_date, "%Y-%m-%d %H:%M:%S.%f"
-                    ).year
-                    age_first_lab = lab_yr - p_birth
-                    return age_first_lab
-
-    raise ValueError("No lab record or no such patient id.")
+    if p_birth == [] or adm_date == []:
+        raise ValueError("No lab record or no such patient id.")
+    return age_first_lab
 
 
 if __name__ == "__main__":
@@ -266,20 +178,14 @@ if __name__ == "__main__":
     print("Enter your lab file name")
     filename_lab = input()
 
-    parse_patient_data(filename_patient)
+    pat_id_list = parse_patient_data(filename_patient)
     parse_lab_data(filename_lab)
-
-    list_patient = create_patient_class()
-    list_lab = create_lab_class()
-    # a = Patient(cur, "A50BE9B4-8A0B-4169-B894-F7BD86D7D90B")
-    # print(a.age, a.gender, a.race, a.DOB)
-    b = Lab(cur, "1A8791E3-A61C-455A-8DEE-763EB90C9B2C", 1, "CBC: MCH")
-    print(b.pat_id,b.lab_value, b.units, b.lab_date)
+    list_of_patient_object = create_patient_class(pat_id_list)
 
     print("Enter the age to calculate:")
     age = float(input())
 
-    num = num_older_than(age, list_patient)
+    num = num_older_than(age, list_of_patient_object)
     print(num)
 
     print('To get the IDs of sick patient, enter the "lab test name" first:')
@@ -289,12 +195,12 @@ if __name__ == "__main__":
     print("Enter critical lab value:")
     value = float(input())
 
-    result_sick_patient = sick_patients(lab, gt_lt, value, list_lab)
+    result_sick_patient = sick_patients(lab, gt_lt, value)
     print(result_sick_patient)
 
     print("Enter the patient ID for age at first admission")
     patient_id = input()
-    result_age_first_adm = age_first_adm(list_patient, list_lab, patient_id)
+    result_age_first_adm = age_first_adm(patient_id)
     print(f"The age of patient at first admission is:", result_age_first_adm)
 
     conn.close()
--- test_EHR.py
+++ test_EHR.py
@@ -1,64 +1,83 @@
-"""Test navigation."""
 import pytest
+import sqlite3
+import os
 
 from EHR_analysis import (
-    parse_data,
+    parse_patient_data,
+    parse_lab_data,
+    create_patient_class,
     num_older_than,
     sick_patients,
     age_first_adm,
 )
 
 
-def test_parse_data():
-    """Test data parsing."""
+from EHR_objects import Patient, Lab
+
+
+def test_parse_patient_data():
+    """Test patient data parsing."""
+    # os.remove("EHR_test.db")
+    conn = sqlite3.connect("EHR.db")
+    cur = conn.cursor()
     Test_Patient_1 = "Test_Patient_1.txt"
+    parsed_patient_data = parse_patient_data(Test_Patient_1)
+    a = Patient(cur, "F0B53A2C-98CA-415D-B928-E3FD0E52B22A")
+    assert a.pat_id == "F0B53A2C-98CA-415D-B928-E3FD0E52B22A"
+    assert a.gender == "Male"
+    assert a.age == 72
+    assert a.race == "African American"
+    assert parsed_patient_data[1] == "F0B53A2C-98CA-415D-B928-E3FD0E52B22A"
+    conn.close()
+
+
+def test_parse_lab_data():
+    """Test lab data parsing."""
+    conn = sqlite3.connect("EHR.db")
+    cur = conn.cursor()
     Test_Lab_1 = "Test_Lab_1.txt"
-    list_of_patient, list_of_lab = parse_data(Test_Patient_1, Test_Lab_1)
-    assert list_of_lab[0].adm_id == str(1)
-    assert list_of_lab[0].lab_value == str(1.8)
-    assert list_of_patient[0].DOB == "1950-06-20 10:31:18.337"
-    assert list_of_patient[0].patient_id == "F0B53A2C-98CA-415D-B928-E3FD0E52B22A"
-    assert list_of_patient[1].patient_id == "1A8791E3-A61C-455A-8DEE-763EB90C9B2A"
+    parse_lab_data(Test_Lab_1)
+    output = cur.execute(
+        "SELECT LabValue FROM lab WHERE PatientID == '1A8791E3-A61C-455A-8DEE-763EB90C9B2C' AND LabName =='CBC: MCH'"
+    ).fetchone()
+    assert output[0] == 35.8
+    conn.close()
 
 
 def test_num_older_than():
     """Test number of patients older."""
+    conn = sqlite3.connect("EHR.db")
+    cur = conn.cursor()
     Test_Patient_1 = "Test_Patient_1.txt"
-    Test_Lab_1 = "Test_Lab_1.txt"
-    parsed_patient_data, _ = parse_data(Test_Patient_1, Test_Lab_1)
-    assert num_older_than(float(60), parsed_patient_data) == 2
-    assert num_older_than(float(80), parsed_patient_data) == 1
+    pat_id_list = parse_patient_data(Test_Patient_1)
+    assert num_older_than(float(60), create_patient_class(pat_id_list)) == 2
+    assert num_older_than(float(80), create_patient_class(pat_id_list)) == 1
+    conn.close()
 
 
 def test_sick_patients():
     """Test ids of sick patients."""
+    conn = sqlite3.connect("EHR.db")
+    cur = conn.cursor()
     Test_Patient_1 = "Test_Patient_1.txt"
     Test_Lab_1 = "Test_Lab_1.txt"
-    _, parsed_lab_data = parse_data(Test_Patient_1, Test_Lab_1)
-    assert sick_patients("METABOLIC: CALCIUM", "<", 8.5, parsed_lab_data) == {
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2A"
+    parse_patient_data(Test_Patient_1)
+    parse_lab_data(Test_Lab_1)
+    assert sick_patients("METABOLIC: CALCIUM", ">", 8.5) == {
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
     }
-    assert sick_patients("METABOLIC: GLUCOSE", "<", 126.0, parsed_lab_data) == {
-        "1A8791E3-A61C-455A-8DEE-763EB90C9B2A"
+    assert sick_patients("METABOLIC: GLUCOSE", "<", 126.0) == {
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
     }
-    assert sick_patients("METABOLIC: GLUCOSE", ">", 126.0, parsed_lab_data) == set([])
+    assert sick_patients("CBC: MCH", ">", 36.0) == set([])
 
 
 def test_age_first_adm():
     """Test age at first admission."""
-    Test_Patient_1 = "Test_Patient_1.txt"
-    Test_Lab_1 = "Test_Lab_1.txt"
-    parsed_patient_data, parsed_lab_data = parse_data(Test_Patient_1, Test_Lab_1)
-
-    assert (
-        age_first_adm(
-            parsed_patient_data, parsed_lab_data, "1A8791E3-A61C-455A-8DEE-763EB90C9B2A"
-        )
-        == 12
-    )
-    assert (
-        age_first_adm(
-            parsed_patient_data, parsed_lab_data, "1A8791E3-A61C-455A-8DEE-763EB90C9B2B"
-        )
-        == 22
-    )
+    conn = sqlite3.connect("EHR.db")
+    cur = conn.cursor()
+    # pat1 = Patient(cursor=cur, pat_id="1A8791E3-A61C-455A-8DEE-763EB90C9B2C")
+    # pat2 = Patient(cursor=cur, pat_id="220C8D43-1322-4A9D-B890-D426942A3649")
+    assert age_first_adm("1A8791E3-A61C-455A-8DEE-763EB90C9B2C") == 19
+    assert age_first_adm("220C8D43-1322-4A9D-B890-D426942A3649") == 24
+    conn.close()
